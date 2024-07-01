60278e964262186798f416ec0056679493577821
CaldatoLuca <lucacaldato15@gmail.com>
2024-07-01 17:41:50 +0200

    feat: modale di cancellazione

--- frontend/src/components/superadmin/DashboardPhotoCard.jsx
+++ frontend/src/components/superadmin/DashboardPhotoCard.jsx
@@ -1,10 +1,30 @@
+import React, { useState } from "react";
 import { useGlobal } from "../../contexts/GlobalContext";
 import { Link } from "react-router-dom";
 import { RiDeleteBinFill as Delete } from "react-icons/ri";
 import { IoPencil as Modify } from "react-icons/io5";
+import Modal from "react-modal";
 
-export default ({ title, image, categories, user, slug, deletePhoto }) => {
+Modal.setAppElement("#root"); // Imposta l'elemento radice dell'app per la modale
+
+export default function PhotoCard({
+  title,
+  image,
+  categories,
+  user,
+  slug,
+  deletePhoto,
+}) {
   const { baseImgUrl } = useGlobal();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  const handleDelete = () => {
+    deletePhoto(slug);
+    closeModal();
+  };
 
   return (
     <div className="col-span-1 shadow-2xl flex flex-col">
@@ -14,7 +34,7 @@ export default ({ title, image, categories, user, slug, deletePhoto }) => {
           <Modify />
         </Link>
         <h3>{title}</h3>
-        <button className="text-red-400" onClick={() => deletePhoto(slug)}>
+        <button className="text-red-400" onClick={openModal}>
           <Delete />
         </button>
       </div>
@@ -48,6 +68,32 @@ export default ({ title, image, categories, user, slug, deletePhoto }) => {
       <div className="text-center p-2 text-lg font-semibold mt-auto">
         {user.name}
       </div>
+
+      {/* Modale di conferma */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Conferma Eliminazione"
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <h2>Are you sure?</h2>
+        <p>This action is irreversable</p>
+        <div className="flex justify-between mt-4">
+          <button
+            onClick={handleDelete}
+            className="text-white bg-red-500 px-4 py-2 rounded"
+          >
+            Delete
+          </button>
+          <button
+            onClick={closeModal}
+            className="text-gray-700 bg-gray-200 px-4 py-2 rounded"
+          >
+            Cancel
+          </button>
+        </div>
+      </Modal>
     </div>
   );
-};
+}
